13db47cdf46321ab0622ad905562fcffe8f3e176
Michael W Powell <mwpowellhtx@gmail.com>
2017-11-03 23:06:40 -0400

    matured the exception handling

--- src/Nanomsg2.Sharp.Tests/Exceptions.cs
+++ src/Nanomsg2.Sharp.Tests/Exceptions.cs
@@ -1,22 +1,32 @@
 using System;
+using System.Linq.Expressions;
 
 namespace Nanomsg2.Sharp
 {
+    using Xunit;
+
     public static class Exceptions
     {
         public delegate bool ExceptionFilter<in T>(T ex)
             where T : Exception;
 
-        public static void Throws<T>(Action action, ExceptionFilter<T> filter)
+        public static void Throws<T>(Action action, Expression<ExceptionFilter<T>> filterExpr)
             where T : Exception
         {
+            var filter = filterExpr.Compile();
             try
             {
                 action();
+                Assert.True(false, $"Expected exception {typeof(T)} to be thrown but was not.");
             }
             catch (T ex) when (filter(ex))
             {
                 // Success. Everything else is a failure.
+            }
+            catch
+            {
+                // It's not perfect, but it will give an indication what's going on.
+                Assert.True(false, $"Expected exception {typeof(T)} to be thrown but was not. Using filter: {filterExpr.Body}");
             }
         }
     }
