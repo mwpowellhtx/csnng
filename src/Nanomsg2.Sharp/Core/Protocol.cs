80e26cfe4ae4284d0cc7295042c850e6f6bf07f4
Michael W Powell <mwpowellhtx@gmail.com>
2017-11-03 20:52:01 -0400

    added protocol middle man class but will leave this out for now

---
+++ src/Nanomsg2.Sharp/Core/Protocol.cs
@@ -0,0 +1,22 @@
+//namespace Nanomsg2.Sharp
+//{
+//    using Protocols;
+
+//    public abstract class Protocol : Invoker, IProtocol
+//    {
+//        //extern int nni_sock_find(nni_sock**, nng::uint32_t);
+//        //extern nng::uint16_t nni_sock_proto(nni_sock*);
+//        //extern nng::uint16_t nni_sock_peer(nni_sock*);
+//        //extern void nni_sock_rele(nni_sock*);
+
+//        private ProtocolType GetSelf()
+//        {
+//        }
+
+//        protected internal abstract uint Id { get; }
+
+//        internal ProtocolType Self { get; }
+
+//        internal ProtocolType Peer { get; }
+//    }
+//}
