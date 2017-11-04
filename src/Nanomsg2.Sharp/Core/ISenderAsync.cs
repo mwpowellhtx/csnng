d4e12ad3fa2daa816cd9816e72ba4c74ba1dbf10
Michael W Powell <mwpowellhtx@gmail.com>
2017-11-03 20:48:34 -0400

    we will circle around to asynchronous methods later

---
+++ src/Nanomsg2.Sharp/Core/ISenderAsync.cs
@@ -0,0 +1,10 @@
+//using System.Threading.Tasks;
+
+//// TODO: TBD: look into this later; will also need the async service that runs along with that...
+//namespace Nanomsg2.Sharp
+//{
+//    public interface ISenderAsync
+//    {
+//        Task SendAsync
+//    }
+//}
