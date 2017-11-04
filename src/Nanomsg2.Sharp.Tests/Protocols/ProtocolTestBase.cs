88008e86b4ccf00a95c75e9fc4c37ca94a772828
Michael W Powell <mwpowellhtx@gmail.com>
2017-11-04 11:18:57 -0400

    added good base functionality to protocol tests

--- src/Nanomsg2.Sharp.Tests/Protocols/ProtocolTestBase.cs
+++ src/Nanomsg2.Sharp.Tests/Protocols/ProtocolTestBase.cs
@@ -1,16 +1,16 @@
 using System;
+using System.Linq;
 
 namespace Nanomsg2.Sharp.Protocols
 {
-    using Messaging;
     using Xunit;
     using Xunit.Abstractions;
 
-    public abstract class ProtocolTestBase<TSocket> : BehaviorDrivenTestFixtureBase
-        where TSocket : Socket, new()
+    public abstract class ProtocolTestBase : BehaviorDrivenTestFixtureBase
 
     {
-        protected static void VerifyDefaultSocket(TSocket s)
+        protected static void VerifyDefaultSocket<T>(T s)
+            where T : Socket, new()
         {
             Assert.NotNull(s);
             Assert.True(s.HasOne);
@@ -23,25 +23,55 @@ namespace Nanomsg2.Sharp.Protocols
         {
         }
 
-        protected static TSocket CreateOne()
+        protected virtual void Given_default_socket<T>()
+            where T : Socket, new()
         {
-            var s = new TSocket();
+            Given_default_socket<T>(s => { });
+        }
+
+        protected virtual void Given_default_socket<T>(Action<T> action)
+            where T : Socket, new()
+        {
+            Section($"given default {typeof(T).FullName} socket", () =>
+            {
+                using (var s = new T())
+                {
+                    action(s);
+                }
+            });
+        }
+
+        protected virtual void That_socket_can_close<T>()
+            where T : Socket, new()
+        {
+            Given_default_socket<T>(s =>
+            {
+                // ReSharper disable once ConvertClosureToMethodGroup
+                Section($" '{typeof(T).FullName}' can {nameof(ISocket.Close)}", () =>
+                {
+                    s.Close();
+                });
+            });
+        }
+
+        protected static T CreateOne<T>()
+            where T : Socket, new()
+        {
+            var s = new T();
             VerifyDefaultSocket(s);
             return s;
+        }
+
+        protected static void ConfigureAll(Action<Socket> action, params Socket[] sockets)
+        {
+            Assert.NotNull(action);
+            sockets.ToList().ForEach(action);
         }
 
         protected static TimeSpan CreateMilliseconds(double value)
         {
             Assert.True(value > 0d || value.Equals(0d));
             return TimeSpan.FromMilliseconds(value);
-        }
-
-        [Fact]
-        public void That_default_socket_correct()
-        {
-            using (var s = CreateOne())
-            {
-            }
         }
     }
 }
