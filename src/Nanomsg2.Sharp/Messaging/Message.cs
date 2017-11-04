9775421b87ccc3fa49a9924433446b1dd289ff93
Michael W Powell <mwpowellhtx@gmail.com>
2017-11-03 20:42:49 -0400

    message can be same as another message fleshed out the invocations a little bit added another constructor

--- src/Nanomsg2.Sharp/Messaging/Message.cs
+++ src/Nanomsg2.Sharp/Messaging/Message.cs
@@ -5,7 +5,7 @@ namespace Nanomsg2.Sharp.Messaging
 {
     using static Imports;
 
-    public class Message : Invoker, IMessage
+    public class Message : Invoker, IMessage, ISameAs<Message>
     {
         private IntPtr _msgPtr = IntPtr.Zero;
 
@@ -24,7 +24,8 @@ namespace Nanomsg2.Sharp.Messaging
             InvokeHavingNoResult(caller, _msgPtr);
         }
 
-        protected internal override void InvokeWithDefaultErrorHandling<T>(InvocationWithResultDelegate<T, int> caller, T ptr)
+        protected internal override void InvokeWithDefaultErrorHandling<T>(InvocationWithResultDelegate<T, int> caller,
+            T ptr)
         {
             Allocate(ref _msgPtr);
             var result = caller(ptr);
@@ -36,7 +37,8 @@ namespace Nanomsg2.Sharp.Messaging
             InvokeWithDefaultErrorHandling(caller, _msgPtr);
         }
 
-        protected internal override TResult InvokeWithResult<T, TResult>(InvocationWithResultDelegate<T, TResult> caller, T ptr)
+        protected internal override TResult InvokeWithResult<T, TResult>(
+            InvocationWithResultDelegate<T, TResult> caller, T ptr)
         {
             Allocate(ref _msgPtr);
             return base.InvokeWithResult(caller, ptr);
@@ -47,7 +49,8 @@ namespace Nanomsg2.Sharp.Messaging
             return InvokeWithResult(caller, _msgPtr);
         }
 
-        protected internal override void InvokeWithDefaultErrorHandling(InvocationWithResultDelegate<IntPtr, int> caller, IntPtr ptr)
+        protected internal override void InvokeWithDefaultErrorHandling(
+            InvocationWithResultDelegate<IntPtr, int> caller, IntPtr ptr)
         {
             Allocate(ref _msgPtr);
             base.InvokeWithDefaultErrorHandling(caller, ptr);
@@ -66,6 +69,12 @@ namespace Nanomsg2.Sharp.Messaging
         public Message()
             : this(0)
         {
+        }
+
+        public Message(IntPtr msgPtr)
+            : this(0)
+        {
+            RetainPtr(msgPtr);
         }
 
         public Message(ulong sz)
@@ -90,6 +99,27 @@ namespace Nanomsg2.Sharp.Messaging
         {
             Header.Clear();
             Body.Clear();
+        }
+
+        private static bool SameAs(Message a, Message b)
+        {
+            return !(a == null || b == null)
+                   && (ReferenceEquals(a, b) || a._msgPtr == b._msgPtr);
+        }
+
+        public virtual bool SameAs(Message other) => SameAs(this, other);
+
+        internal IntPtr CedePtr()
+        {
+            var msgPtr = _msgPtr;
+            _msgPtr = IntPtr.Zero;
+            return msgPtr;
+        }
+
+        internal void RetainPtr(IntPtr msgPtr)
+        {
+            Free();
+            _msgPtr = msgPtr;
         }
 
         protected void Allocate(ref IntPtr msgPtr, ulong sz = 0)
